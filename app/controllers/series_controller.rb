75fa8d39bbd81fa0e648de79965d48af5cf37f9f
GabrielBaezB <gabooo@live.com>
2023-06-27 12:03:48 -0400

    avance de desafio3

---
+++ app/controllers/series_controller.rb
@@ -0,0 +1,26 @@
+class SeriesController < ApplicationController
+  def index
+    @series = Series.all
+  end
+
+  def create
+    @series = Series.new(series_params)
+
+    if @series.save
+      redirect_to series_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @series = Series.new
+  end
+
+  private
+
+  def series_params
+    params.require(:series).permit(:name, :synopsis, :director)
+  end
+end
+  
